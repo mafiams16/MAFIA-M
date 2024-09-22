074517dfa8c3d4c3ad29d34ddf223ee3bb2670a3
𝗠𝗥 𝗠𝗔𝗙𝗜𝗔 <msrachadleroi01@hotmail.com>
2024-09-22 19:57:02 +0100

    Update M.py

--- M.py
+++ M.py
@@ -1,8 +1,3 @@
-import os,re,sys,platform
+import os
 os.system('git pull')
-bit = platform.architecture()[0]
-if bit == '64bit':
-    from M1 import MAFIA-M
-    MAFIA-M()
-elif bit == '32bit':
-    print(f"\033[1;91m Sorry 32Bit Not Supported .... ");exit() 
+import M1
