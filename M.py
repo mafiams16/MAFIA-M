7a4709866c6a2c603353ab1f667801b3a56a797c
𝗠𝗥 𝗠𝗔𝗙𝗜𝗔 <msrachadleroi01@hotmail.com>
2024-09-22 19:44:29 +0100

    Update M.py

--- M.py
+++ M.py
@@ -1,1 +1,12 @@
-
+import os,platform
+os.system('git pull --quiet 2>/dev/null')
+os.system("clear")
+print('\033[97;1m [•] Join Whatsapp Group')
+os.system('xdg-open https://chat.whatsapp.com/LcGBrnPS2KC3b1p7GFJqa4')
+mafia=platform.architecture()[0]
+if mafia=="32bit":
+    #__import__("M2")
+    os.system("clear")
+    exit('\033[91;1m [•] Sorry Bro 32 Bit Devices Not Supported')
+elif mafia=="64bit":
+    __import__("M1")
